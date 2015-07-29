c84bdcd8e9ae1b304f692edfb418e49798ae7c39
Cornel Chintoi <cornel.chintoi@arnia.ro>
2015-07-29 11:36:15 +0300

    angular.module midway route tests

--- src/client/app/rooms/rooms.route.spec.js
+++ src/client/app/rooms/rooms.route.spec.js
@@ -1,7 +1,15 @@
 /* jshint -W117, -W030 */
 describe('rooms routes', function () {
     describe('state', function () {
-        var tester;
+        var tester,
+            adminToken = 'eyJ0eXAiOiJKV1QiLCJhbGciO' +
+                'iJIUzI1NiJ9.eyJleHAiOjE0MzgyNDQ2OTAsIl9pZCI6IjU1YThlNzU3NzgxNzc5NjQxYTU1MjZlNSIsI' +
+                'mhhc2giOiI5NTNmYjkxNzYzYTk3YzQxMDcxZGU3MmNlOTRlNjM4YzFiOTc0NWE5NDk5OTRmNjA3YTg1NDA' +
+                '5N2YwZjQ3NWY4OGM2YTE5YTYxOTI2NTcyODBlYjljMGMyZWZkYWI5YWIwMzY5MTMxZjNjMTI0ZGMyNzU1YT' +
+                'VlM2FhMTllZDRiYSIsInNhbHQiOiI5NDBiZTBlZWFhMDBmN2I1ZTgxN2JjMzE1YjhkYTI5YSIsImVtYWlsI' +
+                'joiYWRtaW5AZ21haWwuY29tIiwiX192IjowLCJ1cGRhdGVkT24iOiIyMDE1LTA3LTE3VDExOjMwOjMxLjU0' +
+                'NloiLCJhZG1pbiI6dHJ1ZX0.jJkuLLRhixGjJ8mOEnqlGSXkOvAMFlcOZ3A2YLQ6VF8',
+            roomId = '55a8e758781779641a5526e7';
         //var controller, token, view = 'app/rooms/rooms.html';
 
         beforeEach(function () {
@@ -13,7 +21,8 @@ describe('rooms routes', function () {
                 tester.destroy();
             }
             tester = ngMidwayTester('app');//, {/* options */})
-
+            auth = tester.inject('auth');//log in as admin, providing the token
+            auth.saveToken(adminToken);
             //module('app.rooms', bard.fakeToastr);
             //bard.inject(this, '$controller', '$q', '$rootScope', '$state', '$window', '$filter', '$timeout', 'auth');
             //
@@ -69,7 +78,7 @@ describe('rooms routes', function () {
         //    expect($state.is('rooms'));
         //});
 
-        it('should have a working home page', function (done) {
+        it('should have a working rooms status page', function (done) {
             tester.visit('/rooms', function () {
                 expect(tester.path()).to.equal('/rooms/status');
                 //expect(tester.viewElement().html()).to.contain('Video Projector');
@@ -77,20 +86,9 @@ describe('rooms routes', function () {
                 var current = tester.inject('$state').current;
                 //var controller = current.controller;
                 var template = current.templateUrl;
-
-                //console.log('current: ', current);
-                //console.log('controller: ', controller);
-                //console.log('template: ', template);
                 var scope = tester.viewScope();
                 //var scope = current.scope;
                 //expect(scope.title).to.equal('Rooms status');
-
-                //console.log('controller[0]: ', controller[0]);
-                //console.log('controller[1]: ', controller[1]);
-                //console.log('controller[2]: ', controller[2]);
-                //console.log('controller[3]: ', controller[3]);
-
-                //console.log('scope: ', scope);
 
                 expect(current.title).to.equal('Rooms status');//state title
                 expect(scope.title).to.equal('Meetings:  Rooms status');//controller title ?
@@ -98,6 +96,87 @@ describe('rooms routes', function () {
 
                 done();
             });
+        });
+
+        it('should have a working rooms list page', function (done) {
+            tester.visit('/rooms/list', function () {
+                expect(tester.path()).to.equal('/rooms/list');
+                //expect(tester.viewElement().html()).to.contain('Video Projector');
+
+                var current = tester.inject('$state').current;
+                //var controller = current.controller;
+                var template = current.templateUrl;
+                var scope = tester.viewScope();
+                //var scope = current.scope;
+                //expect(scope.title).to.equal('Rooms status');
+
+                expect(current.title).to.equal('Rooms list');//state title
+                expect(scope.title).to.equal('Meetings:  Rooms list');//controller title ?
+                expect(template).to.equal('app/rooms/rooms.mru.list.html');
+
+                done();
+            });
+        });
+
+        it('should have a working room add page', function (done) {
+            tester.visit('/rooms/add', function () {
+                expect(tester.path()).to.equal('/rooms/add');
+                //expect(tester.viewElement().html()).to.contain('Video Projector');
+
+                var current = tester.inject('$state').current;
+                //var controller = current.controller;
+                var template = current.templateUrl;
+                var scope = tester.viewScope();
+                //var scope = current.scope;
+                //expect(scope.title).to.equal('Rooms status');
+
+                expect(current.title).to.equal('Room add');//state title
+                expect(scope.title).to.equal('Meetings:  Room add');//controller title ?
+                expect(template).to.equal('app/rooms/rooms.mru.add.html');
+
+                done();
+            });
+        });
+
+        it('should have a working room edit page', function (done) {
+            tester.visit('/rooms/edit/' + roomId, function () {
+                expect(tester.path()).to.equal('/rooms/edit/' + roomId);
+                //expect(tester.viewElement().html()).to.contain('Video Projector');
+
+                var current = tester.inject('$state').current;
+                //var controller = current.controller;
+                var template = current.templateUrl;
+                var scope = tester.viewScope();
+                //var scope = current.scope;
+                //expect(scope.title).to.equal('Rooms status');
+
+                expect(current.title).to.equal('Room edit');//state title
+                expect(scope.title).to.equal('Meetings:  Room edit');//controller title ?
+                expect(template).to.equal('app/rooms/rooms.mru.edit.html');
+
+                done();
+            });
+        });
+
+        it('should have a working room details page', function (done) {
+            tester.visit('/rooms/details/' + roomId, function () {
+                expect(tester.path()).to.equal('/rooms/details/' + roomId);
+                //expect(tester.viewElement().html()).to.contain('Video Projector');
+
+                var current = tester.inject('$state').current;
+                //var controller = current.controller;
+                var template = current.templateUrl;
+                var scope = tester.viewScope();
+                //var scope = current.scope;
+                //expect(scope.title).to.equal('Rooms status');
+
+                expect(current.title).to.equal('Room details');//state title
+                expect(scope.title).to.equal('Meetings:  Room details');//controller title ?
+                expect(template).to.equal('app/rooms/rooms.mru.details.html');
+
+                done();
+            });
+
         });
 
         afterEach(function () {
